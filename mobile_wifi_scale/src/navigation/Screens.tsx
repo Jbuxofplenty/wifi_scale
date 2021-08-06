e7a8000497c536c24b608058a6dc95c9390de811
Josiah <jbuxofplenty@gmail.com>
2021-08-06 17:13:52 -0400

    Changing mobile app template and stripping it down to screens we'll probably need

---
+++ mobile_wifi_scale/src/navigation/Screens.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {Articles, Components, Home, Profile, Register, Pro} from '../screens';
+import {useScreenOptions, useTranslation} from '../hooks';
+
+const Stack = createStackNavigator();
+
+export default () => {
+  const {t} = useTranslation();
+  const screenOptions = useScreenOptions();
+
+  return (
+    <Stack.Navigator screenOptions={screenOptions.stack}>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{title: t('navigation.home')}}
+      />
+
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: false}}
+      />
+
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
